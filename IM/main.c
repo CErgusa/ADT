bd2706cf6483b7e4c7d254c3d398a37448827119
JunsungAhn <junsung.ahn@digipen.edu>
2019-11-25 14:27:18 -0800

    commit for pulling

--- IM/main.c
+++ IM/main.c
@@ -13,9 +13,20 @@
 #include "Xbee.h"
 #include "LCD_SPI.h"
 #include "LCD_UART.h"
+#include "adc.h"
 #include "timer.h"
 #include "tm4c123gh6pm.h"
 
+// PA0 
+// PA1 
+// PB0(UART1 Rx) <--> LCD TXO
+// PB1(UART1 Tx) <--> LCD RXI
+// PD0(AIN7) -> Cell3 -> ADC0
+// PD1(AIN6) -> Cell2 -> ADC0
+// PD2(AIN5) -> Cell1 -> ADC0
+// PD3(AIN4) -> Cell4 -> ADC1
+// PE4(UART5 Rx) <--> Xbee DOUT
+// PE5(UART5 Tx) <--> Xbee DIN
 
 int main(void)
 {
@@ -23,23 +34,28 @@ int main(void)
 	UART0_Init(); // Teraterm
   UART1_Init(); // LCD
   UART5_Init(); // Xbee
+  ADC0_Init();
+  ADC1_Init();
 
-  Xbee_SetDefault();  
+  //Xbee_SetDefault();  
   
   LCD_UART_Init();
   
-  //UART5_OutString("SYSTEM START;");
-  LCD_UART_OutString("SYSTEM START;", true);
-  LCD_UART_OutString("IManager;", false);
+  //UART0_OutString("SYSTEM START;");
+  LCD_UART_OutString("SYSTEM START: IM;", true);
   
   //LCD_UART_Clear();
   // System loop
+  //uint32_t cell = ADC_Get(1, CELL4_CHANNEL); // 127 ~ 170
+  //UART5_InChar(); // Maker sure clear FIFO
 	while (1)
   {
     char input = UART5_InChar();
-    if (input != 0x00)
+    //cell = ADC_Get(1, CELL4_CHANNEL);
+    if (input == 'T')
     {
       LCD_UART_OutChar(input);
+      UART5_OutChar('M');
     }
 	} // End of the system loop
 }
