5e810db02fe551d48071bdca1afd21d7f9df9a66
JunsungAhn <junsung.ahn@digipen.edu>
2020-02-24 08:21:54 -0800

    Delete before working version

--- Tiva/GDL.h
+++
@@ -1,4 +0,0 @@
-#define LIDAR_PACKET_READY 0x01
-
-void GDL_Init(void);
-void GDL_Send(unsigned char data_max5bits);
--- Tiva/main.c
+++
@@ -1,7 +0,0 @@
-#include "system.h"
-
-int main(void)
-{
-	system_init();
-	return system_engine(); // JUN go to system.c for main
-}
